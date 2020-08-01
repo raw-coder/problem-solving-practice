189cf390ade5afc8e82fb7f3e12494ebd1eb2f7f
raw-coder <singular1.steve@gmail.com>
2020-08-01 22:27:43 +0900

    add 1003 9095 11726

---
+++ dynamic-programming/11726.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int dp[1001];
+
+int main() {
+  int i, n;
+  dp[0] = 0;
+  dp[1] = 1;
+  dp[2] = 2;
+  for (i = 3; i <= 1000; i++) {
+    dp[i] = (dp[i - 1] * 2 - 1) % 10007;
+  }
+  scanf("%d", &n);
+  printf("%d", dp[n]);
+  return 0;
+}
