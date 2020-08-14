e363b61f384e688c8ba92f4f7eaeee820916e78d
raw-coder <singular1.steve@gmail.com>
2020-08-14 21:45:08 +0900

    10991

---
+++ input-output/10991.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+  int N, i, j;
+  scanf("%d", &N);
+  for(i = 1; i <= N; i++) {
+    for(j = 1; j <= N - i; j++) {
+      printf(" ");
+    }
+    printf("*");
+    for(j = 2; j <= i; j++) {
+      printf(" *");
+    }
+    printf("\n");
+  }
+  return 0;
+}
