8358d2857a674a9b05697acb3d3d4719c02b290b
raw-coder <singular1.steve@gmail.com>
2020-08-28 12:45:19 +0900

    add 1874

--- stack/1874.cpp
+++ stack/1874.cpp
@@ -1,5 +1,6 @@
+// fixme : vector 이용하여 수정
 #include <stdio.h>
-int a[100001], s = -1;
+int a[100001], s = 0;
 void push(int n) {
   a[++s] = n;
 }
@@ -10,12 +11,13 @@ int top() {
   return a[s];
 }
 int main() {
-  int N, x, i;
+  int N, x, i, idx = 1, ridx = 1;
+  char r[100001];
   scanf("%d", &N);
   for(i = 1; i <= N; i++) {
-    scanf("%d")
+    push(i);
+    scanf("%d", &x);
+    if()
   }
   return 0;
 }
-
-// TODO
