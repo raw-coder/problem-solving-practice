02f9f9f2c4a4df38793c7632d6fc1d430b69e8c6
youngyunjeong <singular1.steve@gmail.com>
2024-07-12 12:22:57 +0900

    add Deleting items from array.py

---
+++ 2.LeetCode/arrays101/Deleting Items from Array.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+        i = 0
+        end = len(nums)
+        while i < end:
+            if nums[i] == val:
+                for j in range(i, end - 1):
+                    nums[j] = nums[j + 1]
+                end -= 1
+                print(i, "changed", nums)
+            else:
+                print(i, "unchanged", nums)
+                k += 1
+                i += 1
+        return k
+
+if __name__ == "__main__":
+    
+    nums = [3,2,3,3]
+    val = 3
+    solution = Solution()
+    print("Input: nums = ", nums, ", val = ", val)
+    result = solution.removeElement(nums, val)
+    print("Output:", result, "nums = ", nums)        
